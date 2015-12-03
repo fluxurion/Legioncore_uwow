db7ca378b63286240c4e31cfd5e1ec27cb2cde1a
moJIto-ice <akriso@i.ua>
2015-12-03 22:29:31 +0300

    SQL

---
+++ sql/uwow/battlenet_components.sql
@@ -0,0 +1,20 @@
+DELETE FROM battlenet_components WHERE Program IN ('Bnet', 'WoW', 'WoWB');
+-- ----------------------------
+-- Records of battlenet_components
+-- ----------------------------
+INSERT INTO `battlenet_components` VALUES
+('Bnet', 'Mc64', '37165'),
+('Bnet', 'Win', '37165'),
+('Bnet', 'Wn64', '37165'),
+('WoW', 'base', '20365'),
+('WoW', 'enGB', '20740'),
+('WoW', 'enUS', '20740'),
+('WoW', 'Mc64', '20365'),
+('WoW', 'ruRU', '20740'),
+('WoW', 'Win', '20740'),
+('WoW', 'Wn64', '20740'),
+('WoWB', 'base', '20365'),
+('WoWB', 'enGB', '20740'),
+('WoWB', 'enUS', '20740'),
+('WoWB', 'Win', '20740'),
+('WoWB', 'Wn64', '20740');
