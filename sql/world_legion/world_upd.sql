fb4cb95ce466ebc9418786a30dfe040eb5ce5444
moJIto-ice <akriso@i.ua>
2016-02-18 19:38:02 +0300

    upd DH start location

--- sql/world_legion/world_upd.sql
+++ sql/world_legion/world_upd.sql
@@ -1,6 +1,8 @@
-INSERT INTO `playercreateinfo` VALUES
-('4', '12', '1', '141', '10311.3', '831.463', '1326.57', '5.69632'),
-('10', '12', '530', '3431', '10349.6', '-6357.29', '33.4026', '5.31605');
+DELETE FROM `playercreateinfo` WHERE (`race`=10 AND `class`=12);
+DELETE FROM `playercreateinfo` WHERE (`race`=4 AND `class`=12);
+INSERT INTO `playercreateinfo` (`race`, `class`, `map`, `position_x`, `position_y`, `position_z`) VALUES
+('4', '12', '1481', '1180.64', '3284.96', '70.5241'),
+('10', '12', '1481', '1180.64', '3284.96', '70.5241');
 
 INSERT INTO `player_levelstats` VALUES
 ('4', '12', '98', '10', '10', '10', '10', '10'),
