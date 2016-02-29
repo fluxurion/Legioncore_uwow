720b5662108b44f8f72f07029678b7990ec40b6a
moJIto-ice <akriso@i.ua>
2016-02-29 15:13:14 +0300

    Revert "read FileDataComplete insted of FileData for GameObjectData extracting; move min/max flightBox calc to core ( reextract maps/mmaps )" && ....

--- src/server/game/Maps/Map.h
+++ src/server/game/Maps/Map.h
@@ -164,8 +164,8 @@ class GridMap
         uint8* m_uint8_V8;
     };
     // Height level data
-    int16* _maxHeight;
-    int16* _minHeight;
+    float* _maxHeight;
+    float* _minHeight;
     float _gridHeight;
     float _gridIntHeightMultiplier;
 
