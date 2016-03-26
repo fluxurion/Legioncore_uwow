16a58fbe6656e49059aacb0b69a6f167a422305d
cyberbrest <d7561985@gmail.com>
2016-03-26 22:44:43 +0300

    [MD]: q40077

--- sql/world_legion/_Mardum_DH.sql
+++ sql/world_legion/_Mardum_DH.sql
@@ -2,6 +2,7 @@
 DELETE FROM `spell_scene` WHERE `SceneScriptPackageID` in (1487, 1451, 1460, 1468, 1470, 1512, 1469, 1479, 1423, 1480, 1493);
 INSERT INTO `spell_scene` (`SceneScriptPackageID`, `MiscValue`, `PlaybackFlags`, MonsterCredit, trigerSpell, ScriptName, comment) VALUES
 (1487, 1106, 20, 0, 0, 'sceneTrigger_dh_init', 'spell 193525 DH_INIT'), -- 1487 - 1106 crash on 701
+(1493, 1116, 27, 0, 0, '', 'spell 191677'), -- 1493 - 1116
 (1451, 1053, 20, 0, 0, '', ''), -- 1451 - 1053
 (1460, 1061, 26, 0, 0, '', ''), -- 1460 - 1061
 (1468, 1077, 20, 0, 0, '', ''), -- 1468 - 1077
@@ -10,13 +11,18 @@ INSERT INTO `spell_scene` (`SceneScriptPackageID`, `MiscValue`, `PlaybackFlags`,
 (1469, 1078, 20, 0, 0, '', ''), -- 1469 - 1078
 (1479, 1094, 20, 0, 0, '', ''), -- 1479 - 1094
 (1423, 1016, 26, 0, 0, '', ''), -- 1423 - 1016
-(1480, 1095, 20, 0, 0, '', ''), -- 1480 - 1095
-(1493, 1116, 27, 0, 0, '', 'spell 191677'); -- 1493 - 1116
+(1480, 1095, 20, 0, 0, '', ''); -- 1480 - 1095
 
 DELETE FROM `spell_area` WHERE area in (7705);
 REPLACE INTO `spell_area` (`area`, `spell`, `quest_start`, `quest_end`, `aura_spell`, `racemask`, `gender`, `autocast`, `quest_start_status`, `quest_end_status`) VALUES 
-('7705', '193525', '0', '40077', '0', '0', '2', '1', '10', '64');
+('7705', '179665', '0', '0', '0', '0', '2', '1', '10', '64'), -- disable all mounts.
+('7705', '193525', '0', '40077', '0', '0', '2', '1', '10', '64'), -- 1106
+('7705', '191677', '0', '40077', '0', '0', '2', '1', '10', '64'); -- 1116
 
 -- 179665 193525
 
 -- Q: 40077
+UPDATE `quest_objectives` SET `ObjectID` = '244898' WHERE `quest_objectives`.`ID` = 280292 AND `QuestID` = 40077;
+REPLACE INTO `gameobject_quest_visual` (`goID`, `questID`, `incomplete_state_spell_visual`, `incomplete_state_world_effect`, `complete_state_spell_visual`, `complete_state_world_effect`, `Comment`)
+ VALUES ('244898', '0', '37794', '0', '8743', '0', 'Legion. Q: 40077');
+UPDATE `gameobject_template` SET `ScriptName` = 'go_q40077' WHERE `gameobject_template`.`entry` = 244898;
--- src/server/newscripts/Legion/mardum.cpp
+++ src/server/newscripts/Legion/mardum.cpp
@@ -4,6 +4,7 @@
 #include "Battleground.h"
 #include "MapManager.h"
 #include "ScriptMgr.h"
+#include "GameObjectAI.h"
 
 class sceneTrigger_dh_init : public SceneTriggerScript
 {
@@ -33,7 +34,52 @@ public:
     }
 };
 
+// 244898
+class go_q40077 : public GameObjectScript
+{
+public:
+    go_q40077() : GameObjectScript("go_q40077") { }
+
+    struct go_q40077AI : public GameObjectAI
+    {
+        go_q40077AI(GameObject* go) : GameObjectAI(go)
+        {
+
+        }
+
+        enum data
+        {
+            SCENE = 191677,
+        };
+
+        bool GossipHello(Player* player) override
+        {
+            /*
+
+            ServerToClient: SMSG_PLAY_SCENE (0x2651) Length: 34 ConnIdx: 0 Time: 02/22/2016 12:57:13.116 Number: 11210
+            SceneID: 1116
+            PlaybackFlags: 27
+            SceneInstanceID: 2
+            SceneScriptPackageID: 1493
+            TransportGUID: Full: 0x0
+            Pos: X: 1002.885 Y: 2956.695 Z: -10.55672
+            Facing: 4.76053
+            */
+            player->CastSpell(player, SCENE, true);
+            player->KillCreditGO(go->GetEntry(), go->GetGUID());
+            return true;
+        }
+
+    };
+
+    GameObjectAI* GetAI(GameObject* go) const
+    {
+        return new go_q40077AI(go);
+    }
+};
+
 void AddSC_Mardum()
 {
     new sceneTrigger_dh_init();
+    new go_q40077();
 }
