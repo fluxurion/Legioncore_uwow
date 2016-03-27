b2f6620fdfa15f07fab1a7c57a12028f3766e39f
cyberbrest <d7561985@gmail.com>
2016-03-27 16:39:48 +0300

    [MD]: misc.

--- sql/world_legion/_Mardum_DH.sql
+++ sql/world_legion/_Mardum_DH.sql
@@ -16,13 +16,18 @@ INSERT INTO `spell_scene` (`SceneScriptPackageID`, `MiscValue`, `PlaybackFlags`,
 DELETE FROM `spell_area` WHERE area in (7705);
 REPLACE INTO `spell_area` (`area`, `spell`, `quest_start`, `quest_end`, `aura_spell`, `racemask`, `gender`, `autocast`, `quest_start_status`, `quest_end_status`) VALUES 
 ('7705', '179665', '0', '0', '0', '0', '2', '1', '10', '64'), -- disable all mounts.
-('7705', '193525', '0', '40077', '0', '0', '2', '1', '10', '64'), -- 1106
-('7705', '191677', '0', '40077', '0', '0', '2', '1', '10', '64'); -- 1116
+('7705', '193525', '0', '40077', '0', '0', '2', '1', '10', '66'), -- 1106
+('7705', '191677', '0', '40077', '0', '0', '2', '0', '10', '64'); -- 1116
 
 -- 179665 193525
 
 -- Q: 40077
 UPDATE `quest_objectives` SET `ObjectID` = '244898' WHERE `quest_objectives`.`ID` = 280292 AND `QuestID` = 40077;
 REPLACE INTO `gameobject_quest_visual` (`goID`, `questID`, `incomplete_state_spell_visual`, `incomplete_state_world_effect`, `complete_state_spell_visual`, `complete_state_world_effect`, `Comment`)
- VALUES ('244898', '0', '37794', '0', '8743', '0', 'Legion. Q: 40077');
+ VALUES ('244898', '40077', '37794', '0', '8743', '0', 'Legion. Q: 40077');
 UPDATE `gameobject_template` SET `ScriptName` = 'go_q40077' WHERE `gameobject_template`.`entry` = 244898;
+
+-- Q: 40378
+
+-- Q: 39279
+REPLACE INTO `area_queststart` (`id`, `quest`) VALUES ('7741', '39279');
