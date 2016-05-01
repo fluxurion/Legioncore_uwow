b02effd46d8013812b2decc00546b5598ea282d0
cyberbrest <d7561985@gmail.com>
2016-05-01 09:50:26 +0300

    [WPR]: Q: 38723

--- sql/world_legion/_dh_quests_p2_warden_prison.sql
+++ sql/world_legion/_dh_quests_p2_warden_prison.sql
@@ -5,20 +5,27 @@
 -- ----------------
 --   P A S E
 -- ----------------
-
 -- 5366 5367   - Q38672
 REPLACE INTO `phase_definitions` (`zoneId`, `entry`, `phasemask`, `phaseId`, `PreloadMapID`, `VisibleMapID`, `flags`, `comment`) VALUES 
 ('7814', '1', '0', '5366 5367', '0', '0', '16', 'While not rew 38672'),
-('7814', '2', '0', '5976', '0', '0', '16', 'rew 38672'),
+('7814', '2', '0', '5976', '0', '0', '16', 'rew 38672'), -- not removed
 ('7814', '3', '0', '5495 5494', '0', '0', '16', 'rew 38672'),
-('7814', '100', '0', '5982 5966 5965 5964 5409 5407 5401 5309 5158 5129 5121 5125 5124', '0', '0', '16', 'Legion. Global.'); --
+('7814', '4', '0', '5128', '0', '0', '16', 'active 38723 and not completed and not rewarded'),
+('7819', '5', '0', '5498', '0', '0', '16', 'rewarded 38723'),
+('7814', '99', '0', '5982 5966 5965 5964 5409 5407 5309 5129 5121 5125 5124', '0', '0', '16', 'Legion. Global.'), --
+('7814', '100', '0', '5401 5158', '0', '0', '16', 'Legion. Global.');
 
-DELETE FROM `conditions` WHERE SourceTypeOrReferenceId = 23 AND SourceGroup = 7814; 
+
+DELETE FROM `conditions` WHERE SourceTypeOrReferenceId = 23 AND SourceGroup in (7814, 7819); 
 INSERT INTO `conditions` (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry`, `SourceId`, `ElseGroup`, `ConditionTypeOrReference`, `ConditionTarget`, `ConditionValue1`, `ConditionValue2`, `ConditionValue3`, `NegativeCondition`, `ErrorTextId`, `ScriptName`, `Comment`) VALUES
 (23, 7814, 1, 0, 0, 8, 0, 38672, 0, 0, 1, 0, '', 'While non rew 38672'),
 (23, 7814, 2, 0, 0, 8, 0, 38672, 0, 0, 0, 0, '', 'While rew 38672'),
-(23, 7814, 3, 0, 0, 14, 0, 38690, 0, 0, 1, 0, '', 'While taken 38690'),
-(23, 7814, 3, 1, 0, 14, 0, 38689, 0, 0, 1, 0, '', 'While taken 38689');
+(23, 7814, 3, 0, 0, 14, 0, 38690, 0, 0, 1, 0, '', 'While not non 38690'),
+(23, 7814, 3, 1, 0, 14, 0, 38689, 0, 0, 1, 0, '', 'While not non 38689'),
+(23, 7814, 4, 0, 0, 9, 0, 38723, 0, 0, 0, 0, '', 'While taken 38723'),
+(23, 7819, 5, 0, 0, 28, 0, 38723, 0, 0, 0, 0, '', 'While completed or rewarded 38723'),
+(23, 7819, 5, 0, 1, 8, 0, 38723, 0, 0, 0, 0, '', 'While completed or rewarded 38723'),
+(23, 7814, 99, 0, 1, 8, 0, 38723, 0, 0, 1, 0, '', 'global not rew 38723');
 
 -- Quest chaine fixes.
 REPLACE INTO `quest_template_addon` (`ID`, `PrevQuestID`, `NextQuestID`, `ExclusiveGroup`) VALUES 
@@ -29,12 +36,12 @@ REPLACE INTO `quest_template_addon` (`ID`, `PrevQuestID`, `NextQuestID`, `Exclus
 ('38690', '38672', '0', '-38723'),
 ('38689', '38672', '0', '-38723'),
 ('38723', '38689', '0', '0'),
--- ToDo
-('39682', '1', '0', '0');
+('39682', '38723', '0', '0');
 
 DELETE FROM `spell_area` WHERE area in (7814);
 REPLACE INTO `spell_area` (`spell`, `area`, `quest_start`, `quest_end`, `aura_spell`, `racemask`, `gender`, `autocast`, `quest_start_status`, `quest_end_status`) VALUES 
-('133508', '7814', '38689', '38689', '0', '0', '2', '1', '10', '64');
+('133508', '7814', '38689', '38689', '0', '0', '2', '1', '10', '64'),
+('187864', '7814', '38723', '38723', '0', '0', '2', '0', '10', '64');
 
 -- Q: 38669 GO 241553
 
@@ -58,3 +65,11 @@ UPDATE `gameobject_template` SET `WorldEffectID` = '4205', `SpellStateAnimKitID`
 UPDATE `creature_template` SET `ScriptName` = 'npc_q38689' WHERE `creature_template`.`entry` in (92782, 92776);
 
 -- Q: 38723
+UPDATE `quest_template` SET `StartScript` = '38723' WHERE `quest_template`.`ID` = 38723;
+DELETE FROM `quest_start_scripts` WHERE id = 38723;
+INSERT INTO `quest_start_scripts` (`id`, `delay`, `command`, `datalong`, `datalong2`, `dataint`, `x`, `y`, `z`, `o`) VALUES 
+('38723', '0', '15', '187864', '2', '0', '0', '0', '0', '0');
+-- ('38723', '1', '6', '1468', '0', '0', '4084.27', '-298.11', '-282.07', '3.118031');
+REPLACE INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES ('187864', 'spell_q38723');
+
+UPDATE `creature_template` SET `ScriptName` = 'npc_q38723' WHERE `creature_template`.`entry` in (92990,97632 );
--- src/server/newscripts/Legion/warden_prison.cpp
+++ src/server/newscripts/Legion/warden_prison.cpp
@@ -117,8 +117,117 @@ public:
     };
 };
 
+//! 187864
+class spell_q38723 : public SpellScriptLoader
+{
+public:
+    spell_q38723() : SpellScriptLoader("spell_q38723") { }
+
+    class spell_q38723_AuraScript : public AuraScript
+    {
+        PrepareAuraScript(spell_q38723_AuraScript);
+
+
+        void OnRemove(AuraEffect const* /*aurEff*/, AuraEffectHandleModes /*mode*/)
+        {
+            Unit* target = GetTarget();
+
+            if (!target || !target->ToPlayer())
+                return;
+            
+            target->ToPlayer()->TeleportTo(1468, 4084.27f, -298.11f, -282.07f, 3.118031f);
+            target->ToPlayer()->KilledMonsterCredit(99303);
+        }
+
+        void Register()
+        {
+            OnEffectRemove += AuraEffectRemoveFn(spell_q38723_AuraScript::OnRemove, EFFECT_0, SPELL_AURA_ACTIVATE_SCENE, AURA_EFFECT_HANDLE_REAL_OR_REAPPLY_MASK);
+        }
+    };
+
+    AuraScript* GetAuraScript() const
+    {
+        return new spell_q38723_AuraScript();
+    }
+};
+
+//! 92990, 97632
+class npc_q38723 : public CreatureScript
+{
+public:
+    npc_q38723() : CreatureScript("npc_q38723") { }
+
+    CreatureAI* GetAI(Creature* creature) const
+    {
+        return new npc_q38723AI(creature);
+    }
+
+    struct npc_q38723AI : public ScriptedAI
+    {
+
+        npc_q38723AI(Creature* creature) : ScriptedAI(creature)
+        {
+            creature->SetCorpseDelay(30);
+            creature->SetRespawnDelay(15);
+        }
+
+        enum data
+        {
+            QUEST = 38723,
+            SPELL_CREDIT = 210461,
+            SPELL_CREDIT2 = 195327,
+        };
+
+        void Reset()
+        {
+            if (me->getVictim() || me->IsInEvadeMode())
+                return;
+
+            std::list<Unit*> list;
+            me->GetAttackableUnitListInRange(list, 70.0f);
+            for (auto enemy : list)
+            {
+                if (enemy->ToPlayer())
+                    continue;
+                me->AI()->AttackStart(enemy);
+                break;
+            }
+        }
+
+        void MoveInLineOfSight(Unit* /*who*/) override {};
+        void DamageDealt(Unit* victim, uint32& damage, DamageEffectType /*damageType*/) override
+        {
+            damage /= 10;
+        }
+
+        void DamageTaken(Unit* attacker, uint32& damage) override
+        {
+            damage = 10000;
+        }
+
+        void JustDied(Unit* /*killer*/)
+        {
+            std::list<Player*> lList;
+            me->GetPlayerListInGrid(lList, 200.0f);
+            for (auto player : lList)
+            {
+                if (player->GetQuestStatus(QUEST) == QUEST_STATUS_INCOMPLETE)
+                {
+                    me->CastSpell(player, SPELL_CREDIT, true);
+                    player->KilledMonsterCredit(106241);
+
+                    
+                    player->CastSpell(player, SPELL_CREDIT2, true);
+                }
+            }
+        }
+
+    };
+};
 void AddSC_warden_prison()
 {
     new go_q38690();
     new npc_q38689();
+    new spell_q38723();
+    new npc_q38723();
 }
